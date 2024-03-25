0cdff3846b32fd98db9235bc51d3813c0a65e36f
Óskar Jörgen Sandholt <oskar.j.sandholt@origo.is>
2024-03-25 13:25:30 +0000

    split naviagtion and harpoon

---
+++ lua/osskari/plugins/harpoon.lua
@@ -0,0 +1,22 @@
+return {
+    "ThePrimeagen/harpoon",
+    branch = "harpoon2",
+    dependencies = {
+        "nvim-lua/plenary.nvim"
+    },
+    config = function()
+        local harpoon = require("harpoon")
+        harpoon:setup()
+
+        vim.keymap.set("n", "<leader>ha", function() harpoon:list():append() end, { desc = "Add current file to harpoon list" })
+        vim.keymap.set("n", "<leader>hh", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end, { desc = "Toggle the harpoon ui" })
+        
+        vim.keymap.set("n", "<leader>hn", function() harpoon:list():prev() end, { desc = "Go to previous hook" })
+        vim.keymap.set("n", "<leader>hp", function() harpoon:list():next() end, { desc = "Go to next hook" })
+
+        vim.keymap.set("n", "<leader>h1", function() harpoon:list():select(1) end, { desc = "Go to hook 1" })
+        vim.keymap.set("n", "<leader>h2", function() harpoon:list():select(2) end, { desc = "Go to hook 2" })
+        vim.keymap.set("n", "<leader>h3", function() harpoon:list():select(3) end, { desc = "Go to hook 3" })
+        vim.keymap.set("n", "<leader>h4", function() harpoon:list():select(4) end, { desc = "Go to hook 4" })
+    end,
+}
--- lua/osskari/remap.lua
+++ lua/osskari/remap.lua
@@ -1,1 +1,8 @@
 -- Put global remaps here
+local keymap = vim.keymap
+
+-- Split navigation
+keymap.set("n", "<C-h>", ":wincmd h<CR>", { desc = "Move to left split" })
+keymap.set("n", "<C-j>", ":wincmd j<CR>", { desc = "Move to lower split" })
+keymap.set("n", "<C-k>", ":wincmd k<CR>", { desc = "Move to upper split" })
+keymap.set("n", "<C-l>", ":wincmd l<CR>", { desc = "Move to right split" })
