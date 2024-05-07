91b5f866dcfa7398de47c6cca5f338aaf41db479
Óskar Jörgen Sandholt <oskar.j.sandholt@origo.is>
2024-05-07 17:11:24 +0000

    created an alias for normal mode o in insert mode

--- lua/osskari/remap.lua
+++ lua/osskari/remap.lua
@@ -1,7 +1,9 @@
--- Put global remaps here
-local keymap = vim.keymap
+-- Put global remaps for NeoVim here
+local map = vim.keymap.set
 
-keymap.set("n", "<leader>wa", ":Wall<CR>", { desc = "Save current buffer" })
+map("n", "<leader>wa", vim.cmd.Wall, { desc = "Save current buffer" })
 
-keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous diagnostic message' })
-keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next diagnostic message' })
+map('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous diagnostic message' })
+map('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next diagnostic message' })
+
+map('i', '<C-Enter>', '<Esc>o', { desc = 'Move cursor to a new line below' })
