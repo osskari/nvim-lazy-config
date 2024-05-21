0f9bf99988c615f1c80c3a0d0b54d936b7ea40dc
Óskar Jörgen Sandholt <oskar.j.sandholt@origo.is>
2024-05-21 10:17:31 +0000

    Added highlight yank back to lua config

--- lua/osskari/remap.lua
+++ lua/osskari/remap.lua
@@ -5,3 +5,12 @@ map("n", "<leader>wa", vim.cmd.Wall, { desc = "Save current buffer" })
 
 map('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous diagnostic message' })
 map('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next diagnostic message' })
+
+-- Highlight on yank
+vim.api.nvim_create_autocmd('TextYankPost', {
+  desc = 'Highlight when yanking (copying) text',
+  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+})
