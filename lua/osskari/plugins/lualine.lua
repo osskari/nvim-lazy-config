3f2a42e25cad90d2c782495e80fb26a997702fe1
Óskar J. Sandholt <oskarjorgen@gmail.com>
2024-08-08 16:35:44 +0000

    lualine: filename now shows relative path from pwd

--- lua/osskari/plugins/lualine.lua
+++ lua/osskari/plugins/lualine.lua
@@ -1,8 +1,14 @@
 return {
   'nvim-lualine/lualine.nvim',
   dependencies = { 'nvim-tree/nvim-web-devicons' },
-  config = function ()
+  config = function()
     require('lualine').setup({
+      sections = {
+        lualine_c = { {
+          'filename',
+          path = 1,
+        } },
+      }
     })
   end
 }
