8e52602d30ec563c5aa1d26858c2382f427fcf1c
Oskar J. Sandholt <oskarjorgen@gmail.com>
2025-01-25 22:10:18 +0000

    migrate keymap to vimscript

--- init.lua
+++ init.lua
@@ -1,5 +1,9 @@
--- require("config.options")
-vim.cmd('source' .. vim.fn.stdpath('config') .. '/vim/osskari/options.vim')
+local function source_file(path)
+  vim.cmd('source' .. vim.fn.stdpath('config') .. path)
+end
+
+source_file('/vim/osskari/options.vim')
 require("config.autocmds")
+source_file('/vim/osskari/keymap.vim')
 require("config.keymap")
 require("config.lazy")
