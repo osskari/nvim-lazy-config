e053ea2f01b04ff4406041ffedbd7d250cad4d61
Óskar J. Sandholt <oskarjorgen@gmail.com>
2025-02-19 12:08:37 +0000

    add deno lsp

--- lua/config/lsp/js.lua
+++ lua/config/lsp/js.lua
@@ -2,6 +2,13 @@ return {
   setup = function (lspconfig, capabilities)
     lspconfig.ts_ls.setup({
       capabilities = capabilities,
+      root_dir = lspconfig.util.root_pattern("package.json"),
+      single_file_support = false,
+    })
+
+    lspconfig.denols.setup({
+      capabilities = capabilities,
+      root_dir = lspconfig.util.root_pattern("deno.json", "deno.jsonc")
     })
   end
 }
