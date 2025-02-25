5ef5eb6684b0c6f80cfb2ea372fb1c646c99d09a
Óskar J. Sandholt <oskarjorgen@gmail.com>
2025-02-25 17:28:53 +0000

    Enable transparency to match terminal bg

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,31 +1,14 @@
 return {
-  -- {
-  --   "folke/tokyonight.nvim",
-  --   -- lazy = true,
-  --   opts = { style = "moon" },
-  --   config = function()
-  --     vim.cmd("colorscheme tokyonight")
-  --     vim.api.nvim_set_hl(0, "ColorColumn", { bg = "#292e42" })
-  --   end,
-  -- },
-  -- {
-  --   "ellisonleao/gruvbox.nvim",
-  --   priority = 1000,
-  --   opts = ...,
-  --   config = function ()
-  --     local gruvbox = require("gruvbox")
-  --
-  --     gruvbox.setup({})
-  --
-  --     vim.o.background = "dark"
-  --     vim.cmd([[colorscheme gruvbox]])
-  --   end
-  -- },
-  -- Lazy
   {
     "olimorris/onedarkpro.nvim",
     priority = 1000, -- Ensure it loads first
     config = function ()
+      require("onedarkpro").setup({
+        options = {
+          transparency = true
+        }
+      })
+
       vim.cmd("colorscheme onedark")
     end
   }
