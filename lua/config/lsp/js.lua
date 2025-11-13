fd995d1f71d0341b0d633a1d43124aa159eda440
Óskar J. Sandholt <oskarjorgen@gmail.com>
2025-11-13 12:54:56 +0000

    remove denols

--- lua/config/lsp/js.lua
+++ lua/config/lsp/js.lua
@@ -5,10 +5,5 @@ return {
       root_dir = lspconfig.util.root_pattern("package.json"),
       single_file_support = false,
     })
-
-    lspconfig.denols.setup({
-      capabilities = capabilities,
-      root_dir = lspconfig.util.root_pattern("deno.json", "deno.jsonc")
-    })
   end
 }
