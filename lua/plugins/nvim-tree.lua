21673b637a8c05014865ef3a00fbe70b68745288
Óskar J. Sandholt <oskarjorgen@gmail.com>
2025-11-26 17:15:55 +0000

    modify leader ef for my very specific needs

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -3,7 +3,7 @@ return {
   dependencies = {
     "nvim-tree/nvim-web-devicons"
   },
-  config = function ()
+  config = function()
     local nvimtree = require("nvim-tree")
 
     vim.g.loaded_netrw = 1
@@ -20,7 +20,22 @@ return {
       },
     })
 
+    local api = require("nvim-tree.api")
+
     vim.keymap.set('n', '<leader>ee', '<CMD>NvimTreeToggle<CR>')
-    vim.keymap.set('n', '<leader>ef', '<CMD>NvimTreeFindFileToggle<CR>')
+    vim.keymap.set('n', '<leader>ef', function()
+      if (not api.tree.is_visible() or not vim.fn.expand('%'):match('^NvimTree_*')) then
+        api.tree.open({ find_file = true })
+        return
+      end
+
+      local previous_file = vim.fn.expand('#'):match("([^/\\]+)$")
+
+      if (previous_file == nil or api.tree.get_node_under_cursor().name == previous_file) then
+        api.tree.close()
+      else
+        api.tree.find_file({ buf = vim.fn.bufnr('#') })
+      end
+    end)
   end
 }
