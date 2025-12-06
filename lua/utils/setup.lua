ee66febb11145af549d725d43496310e8c58631f
Óskar Jörgen Sandholt <oskarjorgen@gmail.com>
2025-12-06 15:17:00 +0000

    remove options reference

--- lua/utils/setup.lua
+++ lua/utils/setup.lua
@@ -5,7 +5,6 @@ local M = {}
 M.setup = function()
   paths.require_vim('config.globals')
   paths.require_vim('config.keymaps')
-  paths.require_vim("config.options")
   require("config.options")
   require("config.autocmds")
 end
