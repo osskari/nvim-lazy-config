584c54f56f7b50502c043d7aff9d8520aa343387
Óskar J. Sandholt <oskarjorgen@gmail.com>
2026-03-29 19:01:06 +0000

    add cmp

--- init.lua
+++ init.lua
@@ -68,5 +68,59 @@ vim.keymap.set("n", "gr", function() Snacks.picker.lsp_references() end, { desc 
 vim.keymap.set("n", "gd", function() Snacks.picker.lsp_definitions() end, { desc = "Go to definition" })
 
 -- cmp
+MiniDeps.add({
+  source = "hrsh7th/nvim-cmp",
+  depends = {
+    "hrsh7th/cmp-nvim-lsp",
+    "hrsh7th/cmp-buffer",
+    "hrsh7th/cmp-path",
+    "hrsh7th/cmp-nvim-lsp-signature-help",
+    "rafamadriz/friendly-snippets",
+    "saadparwaiz1/cmp_luasnip",
+    {
+      source = "L3MON4D3/LuaSnip",
+      checkout = "v2.4.1",
+      hooks = {
+        post_install = function(spec)
+          vim.fn.system({"make", "install_jsregexp"}, {cwd = spec.path})
+        end
+      }
+    },
+  },
+})
+local cmp = require("cmp")
+
+cmp.setup({
+
+  snippet = {
+    expand = function(args)
+      require('luasnip').lsp_expand(args.body)
+    end
+  },
+
+  window = {
+    completion = cmp.config.window.bordered(),
+    documentation = cmp.config.window.bordered(),
+  },
+
+  mapping = cmp.mapping.preset.insert({
+    ["<C-k>"] = cmp.mapping.select_prev_item(),
+    ["<C-j>"] = cmp.mapping.select_next_item(),
+    ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+    ["<C-f>"] = cmp.mapping.scroll_docs(4),
+    ["<C-Space>"] = cmp.mapping.complete(),
+    ["<C-e>"] = cmp.mapping.abort(),
+    ["<CR>"] = cmp.mapping.confirm({ select = false }),
+    ["<TAB>"] = cmp.mapping.confirm({ select = true }),
+  }),
+
+  sources = {
+    { name = "luasnip" },
+    { name = "nvim_lsp" },
+    { name = "buffer" },
+    { name = "path" },
+    { name = "nvim_signature_help" },
+  },
+})
 
 -- lsp
