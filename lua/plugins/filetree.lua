d3a5621780c6f3ec9dd480eabe40d2102a93b017
osskari <oskarjorgen@gmail.com>
2026-09-09 22:36:48 +0000

    find file tree bind

--- lua/plugins/filetree.lua
+++ lua/plugins/filetree.lua
@@ -5,8 +5,24 @@ vim.pack.add({"https://github.com/nvim-tree/nvim-tree.lua", "https://github.com/
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
-require("nvim-tree").setup()
+require("nvim-tree").setup({
+  filters = {
+    git_ignored = false,
+  }
+})
 
 local api = require("nvim-tree.api")
 
-vim.keymap.set("n", "<leader>e", api.tree.toggle)
+vim.keymap.set("n", "<leader>ee", api.tree.toggle)
+vim.keymap.set("n", "<leader>ef", function ()
+  -- if in a tree toggle it
+  if api.tree.is_tree_buf() then
+    api.tree.toggle()
+    return
+  end
+  -- if not in tree open it highlighting the current file
+  api.tree.find_file({
+    focus = true,
+    open = true,
+  })
+end)
