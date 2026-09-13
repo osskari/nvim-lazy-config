5da4f6b1834ddd4720af4c088eda7dca6f656ef1
osskari <oskarjorgen@gmail.com>
2026-09-13 23:01:34 +0000

    numbered binds harpoon

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -24,3 +24,32 @@ end)
 vim.keymap.set("n", "<leader>hn", function()
   harpoon:list():next()
 end)
+
+
+vim.keymap.set("n", "<leader>h1", function ()
+  harpoon:list():select(1)
+end)
+
+vim.keymap.set("n", "<leader>h2", function ()
+  harpoon:list():select(2)
+end)
+
+vim.keymap.set("n", "<leader>h3", function ()
+  harpoon:list():select(3)
+end)
+
+vim.keymap.set("n", "<leader>h4", function ()
+  harpoon:list():select(4)
+end)
+
+vim.keymap.set("n", "<leader>h5", function ()
+  harpoon:list():select(5)
+end)
+
+vim.keymap.set("n", "<leader>h6", function ()
+  harpoon:list():select(6)
+end)
+
+vim.keymap.set("n", "<leader>h7", function ()
+  harpoon:list():select(7)
+end)
